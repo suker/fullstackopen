ea3fb5f296d01b814903efcf368c5b7346f2a742
Juan Carlos <suker_juan@hotmail.com>
2024-08-12 12:56:19 +0200

    [ADD] prettier conf [ADD] part2: The phonebook step 3  Adding number input and display it

--- part2/phonebook/main.jsx
+++ part2/phonebook/main.jsx
@@ -2,4 +2,4 @@ import { createRoot } from 'react-dom/client'
 import App from './src/App.jsx'
 
 const root = createRoot(document.getElementById('app'))
-root.render(<App/>)
+root.render(<App />)
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,45 +1,67 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+	const [persons, setPersons] = useState([
+		{
+			name: 'Arto Hellas',
+			number: '738421201',
+		},
+	]);
+	const [newName, setNewName] = useState('');
+	const [newNumber, setNewNumber] = useState('');
 
-  const addContact = (e) => {
-    e.preventDefault()
-    const newPerson = {
-        name: newName,
-    }
-    const repeatedName = persons.find((person) => JSON.stringify(
-        person) === JSON.stringify(newPerson))
+	const addContact = (e) => {
+		e.preventDefault();
+		const newPerson = {
+			name: newName,
+			number: newNumber,
+		};
+		const repeatedName = persons.find(
+			(person) => JSON.stringify(person) === JSON.stringify(newPerson)
+		);
 
-    if (repeatedName) {
-        alert(`${newPerson.name} is already added to phonebook`)
-        return 
-    }
-    setPersons([...persons, newPerson])
-    setNewName('')
-  }
+		if (repeatedName) {
+			alert(`${newPerson.name} is already added to phonebook`);
+			return;
+		}
+		setPersons([...persons, newPerson]);
+		setNewName('');
+		setNewNumber('');
+	};
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
-      <form onSubmit={addContact}>
-        <div>
-          name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
-        </div>
-        <div>
-          <button type="submit" >add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.map((person) => <li key={person.name}>{person.name}</li>)}
-      </ul>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<div>debug: {newName}</div>
+			<form onSubmit={addContact}>
+				<div>
+					name:{' '}
+					<input
+						value={newName}
+						onChange={(e) => setNewName(e.target.value)}
+					/>
+				</div>
+				<div>
+					number:{' '}
+					<input
+						value={newNumber}
+						onChange={(e) => setNewNumber(e.target.value)}
+					/>
+				</div>
+				<div>
+					<button type="submit">add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			<ul>
+				{persons.map((person) => (
+					<li key={person.name}>
+						{person.name} {person.number}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default App;
