264c85b0e125d50e58ed09d7f195026e2fca2f45
Juan Carlos <suker_juan@hotmail.com>
2024-08-12 18:21:22 +0200

    [ADD] part2: The Phonebook step 6:  Use of axios to get data from db.json file  through local json-server

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Filter = ({ filterProps }) => {
     const [filterContact, setFilterContact] = filterProps
@@ -50,15 +51,20 @@ const Persons = ({ persons }) => {
 };
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-      ])
+    const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('');
 	const [newNumber, setNewNumber] = useState('');
 	const [filterContact, setFilterContact] = useState('');
+
+	useEffect(()=> {
+		console.log('Effect')
+		axios
+		.get('http://localhost:3001/persons')
+		.then(resp => {
+			console.log(resp)
+			setPersons(resp.data)
+		})
+	}, [])
 
 	const addContact = (e) => {
 		e.preventDefault();
