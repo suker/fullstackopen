f6fc063b36beaed0c375cafee1385f0f8384842f
Juan Carlos <suker_juan@hotmail.com>
2024-09-11 14:37:29 +0200

    [ADD] 4.5*: Helper Functions and Unit Tests, step 3  * New favoriteBlog function to test.  * It finds out which blog has the most likes.

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -7,4 +7,22 @@ const totalLikes = (blogs) => {
 	return blogs.map((blog) => blog.likes).reduce((sum, value) => sum + value);
 };
 
-module.exports = { dummy, totalLikes };
+const favoriteBlog = (blogs) => {
+	let mostLikes = 0;
+	let favoriteBlog = {};
+
+	blogs.forEach((blog) => {
+		if (blog.likes > mostLikes) {
+			mostLikes = blog.likes;
+			favoriteBlog = blog;
+		}
+	});
+
+	delete favoriteBlog._id;
+	delete favoriteBlog.url;
+	delete favoriteBlog.__v;
+
+	return favoriteBlog;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
