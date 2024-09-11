6c16c30586852a11c65440df6a2d2922882e0511
Juan Carlos <suker_juan@hotmail.com>
2024-09-11 14:21:44 +0200

    [ADD] 4.4: Helper Functions and Unit Tests, step 2  * New totalLikes function that counts number of    likes on a blog list  * Implement testing for it

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,4 +3,8 @@ const dummy = (blogs) => {
 	return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+	return blogs.map((blog) => blog.likes).reduce((sum, value) => sum + value);
+};
+
+module.exports = { dummy, totalLikes };
