ca4e895b76bcaefc16f0ff66617af3d5f5b4336b
Juan Carlos <suker_juan@hotmail.com>
2024-09-11 17:59:28 +0200

    [ADD] 4.6*: Helper Functions and Unit Tests, step 4  * Test mostBlogs and mostLikes functions

--- part4/bloglist/tests/dummy.test.js
+++ part4/bloglist/tests/dummy.test.js
@@ -1,7 +1,13 @@
 const { test, describe } = require('node:test');
 const assert = require('node:assert');
 
-const { dummy, totalLikes, favoriteBlog } = require('../utils/list_helper');
+const {
+	dummy,
+	totalLikes,
+	favoriteBlog,
+	mostBlogs,
+    mostLikes,
+} = require('../utils/list_helper');
 
 test('dummy returns one', () => {
 	const blogs = [];
@@ -11,56 +17,55 @@ test('dummy returns one', () => {
 });
 
 const listWithMultipleBlogs = [
-    {
-      _id: "5a422a851b54a676234d17f7",
-      title: "React patterns",
-      author: "Michael Chan",
-      url: "https://reactpatterns.com/",
-      likes: 7,
-      __v: 0
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-      likes: 5,
-      __v: 0
-    },
-    {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
-    },
-    {
-      _id: "5a422b891b54a676234d17fa",
-      title: "First class tests",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-      likes: 10,
-      __v: 0
-    },
-    {
-      _id: "5a422ba71b54a676234d17fb",
-      title: "TDD harms architecture",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
-      __v: 0
-    },
-    {
-      _id: "5a422bc61b54a676234d17fc",
-      title: "Type wars",
-      author: "Robert C. Martin",
-      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-      likes: 2,
-      __v: 0
-    }  
-  ]
-
+	{
+		_id: '5a422a851b54a676234d17f7',
+		title: 'React patterns',
+		author: 'Michael Chan',
+		url: 'https://reactpatterns.com/',
+		likes: 7,
+		__v: 0,
+	},
+	{
+		_id: '5a422aa71b54a676234d17f8',
+		title: 'Go To Statement Considered Harmful',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+		likes: 5,
+		__v: 0,
+	},
+	{
+		_id: '5a422b3a1b54a676234d17f9',
+		title: 'Canonical string reduction',
+		author: 'Edsger W. Dijkstra',
+		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+		likes: 12,
+		__v: 0,
+	},
+	{
+		_id: '5a422b891b54a676234d17fa',
+		title: 'First class tests',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+		likes: 10,
+		__v: 0,
+	},
+	{
+		_id: '5a422ba71b54a676234d17fb',
+		title: 'TDD harms architecture',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+		likes: 0,
+		__v: 0,
+	},
+	{
+		_id: '5a422bc61b54a676234d17fc',
+		title: 'Type wars',
+		author: 'Robert C. Martin',
+		url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+		likes: 2,
+		__v: 0,
+	},
+];
 
 describe('total likes', () => {
 	const listWithOneBlog = [
@@ -79,22 +84,42 @@ describe('total likes', () => {
 		assert.strictEqual(result, 5);
 	});
 
-
-    test('when list has multiple blog, equals the likes of that', () => {
+	test('when list has multiple blog, equals the likes of that', () => {
 		const result = totalLikes(listWithMultipleBlogs);
 		assert.strictEqual(result, 36);
 	});
 });
 
+describe('Most likes', () => {
+	const result = {
+		title: 'Canonical string reduction',
+		author: 'Edsger W. Dijkstra',
+		likes: 12,
+	};
+
+	test('blog with most likes', () => {
+		assert.deepStrictEqual(favoriteBlog(listWithMultipleBlogs), result);
+	});
+});
+
+describe('Most blogs', () => {
+	const result = {
+		author: 'Robert C. Martin',
+		blogs: 3,
+	};
+
+	test('Author with most blogs written', () => {
+		assert.deepStrictEqual(mostBlogs(listWithMultipleBlogs), result);
+	});
+});
 
 describe('Most likes', () => {
-    const result = {
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        likes: 12,
-      }
+	const result = {
+		author: 'Edsger W. Dijkstra',
+		likes: 17,
+	};
 
-    test('blog with most likes', () => {
-        assert.deepStrictEqual(favoriteBlog(listWithMultipleBlogs), result)
-    })
-})
+	test('Author with most likes in its blogs', () => {
+		assert.deepStrictEqual(mostLikes(listWithMultipleBlogs), result);
+	});
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -25,4 +25,43 @@ const favoriteBlog = (blogs) => {
 	return favoriteBlog;
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog };
+// returns the author who has the largest amount of blogs
+const mostBlogs = (blogs) => {
+
+    const authorCounts = blogs.reduce((acc, blog) => {
+        acc[blog.author] = (acc[blog.author] || 0) + 1
+        return acc
+    }, {})
+
+    const mostBlogsAuthor = Object.keys(authorCounts).reduce((a, b) => {
+        return authorCounts[a] > authorCounts[b] ? a : b
+    }, 0)
+
+    return {
+        author: mostBlogsAuthor,
+        blogs: authorCounts[mostBlogsAuthor]
+    }
+}
+
+const mostLikes = (blogs) => {
+
+    const likesCount = blogs.reduce((acc, blog) => {
+        console.log(blog.likes)
+        if (acc[blog.author]) {
+         acc[blog.author] += blog.likes
+        } else 
+          acc[blog.author] = (blog.likes || 0)
+        return acc
+    }, {})
+
+    const mostBlogsAuthor = Object.keys(likesCount).reduce((a, b) => {
+        return likesCount[a] > likesCount[b] ? a : b
+    }, 0)
+
+    return {
+        author: mostBlogsAuthor,
+        likes: likesCount[mostBlogsAuthor]
+    }
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes };
