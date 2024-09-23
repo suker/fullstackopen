5eff0734129354a3f0446cdf26499c314226117d
Juan Carlos <suker_juan@hotmail.com>
2024-09-23 15:43:12 +0200

    [ADD] 4.18: Blog List Expansion, step 6&7  Implement token-based authentication.  Modify adding new blogs so that it is  only possible if a valid token is sent  with the HTTP POST request.  The user identified by the token is  designated as the creator of the blog.

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const middleware = require('./utils/middleware.js');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const mongoose = require('mongoose');
 const { MONGODB_URI } = require('./utils/config.js');
 
@@ -18,6 +19,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
+const getTokenFrom = (request) => {
+	const authorization = request.get('authorization');
+	if (authorization && authorization.startsWith('Bearer ')) {
+		return authorization.replace('Bearer ', '');
+	}
+	return null;
+};
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({}).populate('user', ['username', 'name']);
@@ -10,13 +19,17 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
 	const body = request.body;
 
+	const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+	if (!decodedToken.id) {
+		return response.status(401).json({ error: 'token invalid' });
+	}
+	const user = await User.findById(decodedToken.id);
+
 	if (!body.title || !body.url) {
 		return response.status(400).json({
 			error: 'title or url missing',
 		});
 	}
-
-	const user = (await User.find({}))[0];
 
 	const blog = new Blog({
 		title: body.title,
