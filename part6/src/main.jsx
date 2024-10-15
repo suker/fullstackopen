0fe1b3de3e26448c4ead2c1ef3cfdc900f7feecb
Juan Carlos <suker_juan@hotmail.com>
2024-10-15 13:21:25 +0200

    [ADD] Exercises 6.1.-6.2.  Implementing flux arquitecture to manage  app state of unicafe reviews with redux

---
+++ part6/src/main.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+const store = createStore(reducer);
+
+const App = () => {
+	const handleGood = () => {
+		store.dispatch({
+			type: 'GOOD',
+		});
+	};
+
+	const handleOk = () => {
+		store.dispatch({
+			type: 'OK',
+		});
+	};
+
+  const handleBad = () => {
+		store.dispatch({
+			type: 'BAD',
+		});
+	};
+
+  const handleReset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+	return (
+		<div>
+			<button onClick={handleGood}>good</button>
+			<button onClick={handleOk}>ok</button>
+			<button onClick={handleBad}>bad</button>
+			<button onClick={handleReset}>reset stats</button>
+			<div>good {store.getState().good}</div>
+			<div>ok {store.getState().ok}</div>
+			<div>bad {store.getState().bad}</div>
+		</div>
+	);
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const renderApp = () => {
+	root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
